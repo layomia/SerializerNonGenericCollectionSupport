584b7ac9fc2fa5a12ae5c1911ab90f3d907d20ae
Layomi Akinrinade <laakinri@microsoft.com>
2019-06-05 08:11:08 -0400

    Init commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SerializerNonGenericCollectionSupport
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
