0fa227b60ef0d4c4c4146b96e4d91ddb623098f9
Layomi Akinrinade <laakinri@microsoft.com>
2019-06-14 17:33:27 -0400

    New perf

--- Program.cs
+++ Program.cs
@@ -15,7 +15,10 @@ namespace SerializerNonGenericCollectionSupport
         private static IEnumerable _ienumerable = new List<string>();
         private static ICollection _icollection = new List<string>();
 
-        private static string _jsonString;
+        private static readonly IDictionary _idictionary = new Dictionary<string, string>();
+
+        private static string _jsonListString;
+        private static string _jsonDictString;
 
         [Params(2, 50, 100)]
         public int ElementCount;
@@ -26,49 +29,124 @@ namespace SerializerNonGenericCollectionSupport
             for (int i = 0; i < ElementCount; i++)
             {
                 _ilist.Add($"hello{i}");
+                _idictionary.Add($"hello{i}", $"world{i}");
             }
 
             _ienumerable = _ilist;
             _icollection = _ilist;
 
-            _jsonString = System.Text.Json.Serialization.JsonSerializer.ToString(_ilist);
+            _jsonListString = System.Text.Json.JsonSerializer.ToString(_ilist);
+            _jsonDictString = System.Text.Json.JsonSerializer.ToString(_idictionary);
+        }
+
+        //[Benchmark]
+        //public IEnumerable DeserializeIEnumerable()
+        //{
+        //    return System.Text.Json.JsonSerializer.Parse<IEnumerable>(_jsonListString);
+        //}
+
+        //[Benchmark]
+        //public IList DeserializeIList()
+        //{
+        //    return System.Text.Json.JsonSerializer.Parse<IList>(_jsonListString);
+        //}
+
+        //[Benchmark]
+        //public ICollection DeserializeICollection()
+        //{
+        //    return System.Text.Json.JsonSerializer.Parse<ICollection>(_jsonListString);
+        //}
+
+        //[Benchmark]
+        //public IList<string> DeserializeGenericIList()
+        //{
+        //    return System.Text.Json.JsonSerializer.Parse<IList<string>>(_jsonListString);
+        //}
+
+
+        [Benchmark]
+        public Stack DeserializeStack()
+        {
+            return System.Text.Json.JsonSerializer.Parse<Stack>(_jsonListString);
         }
 
         [Benchmark]
-        public IEnumerable DeserializeIEnumerable()
+        public Queue DeserializeQueue()
         {
-            return System.Text.Json.Serialization.JsonSerializer.Parse<IEnumerable>(_jsonString);
+            return System.Text.Json.JsonSerializer.Parse<Queue>(_jsonListString);
         }
 
         [Benchmark]
-        public IList DeserializeIList()
+        public ArrayList DeserializeArrayList()
         {
-            return System.Text.Json.Serialization.JsonSerializer.Parse<IList>(_jsonString);
+            return System.Text.Json.JsonSerializer.Parse<ArrayList>(_jsonListString);
         }
 
         [Benchmark]
-        public ICollection DeserializeICollection()
+        public Stack<string> DeserializeGenericStack()
         {
-            return System.Text.Json.Serialization.JsonSerializer.Parse<ICollection>(_jsonString);
+            return System.Text.Json.JsonSerializer.Parse<Stack<string>>(_jsonListString);
         }
 
         [Benchmark]
-        public byte[] SerializeIList_ToBytes()
+        public Queue<string> DeserializeGenericQueue()
         {
-            return System.Text.Json.Serialization.JsonSerializer.ToUtf8Bytes(_ilist);
+            return System.Text.Json.JsonSerializer.Parse<Queue<string>>(_jsonListString);
         }
 
         [Benchmark]
-        public byte[] SerializeIEnumerable_ToBytes()
+        public HashSet<string> DeserializeGenericHashSet()
         {
-            return System.Text.Json.Serialization.JsonSerializer.ToUtf8Bytes(_ienumerable);
+            return System.Text.Json.JsonSerializer.Parse<HashSet<string>>(_jsonListString);
         }
 
         [Benchmark]
-        public byte[] SerializeICollection_ToBytes()
+        public LinkedList<string> DeserializeGenericLinkedList()
         {
-            return System.Text.Json.Serialization.JsonSerializer.ToUtf8Bytes(_icollection);
+            return System.Text.Json.JsonSerializer.Parse<LinkedList<string>>(_jsonListString);
         }
+
+        [Benchmark]
+        public SortedSet<string> DeserializeGenericSortedSet()
+        {
+            return System.Text.Json.JsonSerializer.Parse<SortedSet<string>>(_jsonListString);
+        }
+
+        //[Benchmark]
+        //public IDictionary DeserializeIDictionary()
+        //{
+        //    return System.Text.Json.JsonSerializer.Parse<IDictionary>(_jsonDictString);
+        //}
+
+        [Benchmark]
+        public IDictionary DeserializeHashtable()
+        {
+            return System.Text.Json.JsonSerializer.Parse<Hashtable>(_jsonDictString);
+        }
+
+        [Benchmark]
+        public IDictionary DeserializeSortedList()
+        {
+            return System.Text.Json.JsonSerializer.Parse<SortedList>(_jsonDictString);
+        }
+
+        //[Benchmark]
+        //public byte[] SerializeIList_ToBytes()
+        //{
+        //    return System.Text.Json.JsonSerializer.ToUtf8Bytes(_ilist);
+        //}
+
+        //[Benchmark]
+        //public byte[] SerializeIEnumerable_ToBytes()
+        //{
+        //    return System.Text.Json.JsonSerializer.ToUtf8Bytes(_ienumerable);
+        //}
+
+        //[Benchmark]
+        //public byte[] SerializeICollection_ToBytes()
+        //{
+        //    return System.Text.Json.JsonSerializer.ToUtf8Bytes(_icollection);
+        //}
     }
 
     [JsonExporterAttribute.Full]
@@ -95,7 +173,7 @@ namespace SerializerNonGenericCollectionSupport
             _ienumerable = _ilist;
             _icollection = _ilist;
 
-            _jsonString = System.Text.Json.Serialization.JsonSerializer.ToString(_ilist);
+            _jsonString = System.Text.Json.JsonSerializer.ToString(_ilist);
         }
 
         [Benchmark]
